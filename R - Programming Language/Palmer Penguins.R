05696e8544c41d34d905c6a33f4d7aa503747152
Sumaiya Tarannum Noor <sumaiyatarannumnoor001@gmail.com>
2024-12-26 11:49:21 +0600

    Add files via upload

---
+++ R - Programming Language/Palmer Penguins.R
@@ -0,0 +1,7 @@
+# install.packages("palmerpenguins")
+library(palmerpenguins)
+summary(penguins)
+summary(penguins_raw)
+View(penguins)
+library(ggplot2)
+ggplot(data=penguins, aes(x=flipper_length_mm, y=body_mass_g))+geom_point(aes(color=species))
