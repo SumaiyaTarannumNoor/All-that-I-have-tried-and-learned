f67ba92ee71283af87d189fc66be1c90c814e671
Sumaiya Tarannum Noor <sumaiyatarannumnoor001@gmail.com>
2024-12-26 12:45:46 +0600

    Add files via upload

---
+++ R - Programming Language/Fundamentals.R
@@ -0,0 +1,12 @@
+# Examples 
+First_variable <- "This is a variable."
+Second_varible <- 12.5
+
+# Vector(R) - A group of data elements of the same type stored in a sequence in R.
+
+Vector_1 <- c(13, 14, 15, 16)
+Vector_1 
+
+# Pipe (R) - A Pipe is a tool in R for expressing a sequence of multiple operations, represented with "%>%".
+
+
